1cce2e491bc68d4081ffe17096b4e0b0363d9ce8
Boasbabs <babalolasimeon@gmail.com>
2018-02-23 20:29:12 +0000

    Create views, templates, URL for home page & add static files

--- mozilla_blog/blog/urls.py
+++ mozilla_blog/blog/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # adding path to the story
+    path("", include("story.urls")),
+    # redirecting blog path
+    path("blog/", RedirectView.as_view(url=""))
+
 ]
