9eec3c65afe933397bb9125d38fb8d144e29a58d
Boasbabs <babalolasimeon@gmail.com>
2018-02-27 22:43:36 +0000

    little commit

--- mozilla_blog/blog/settings.py
+++ mozilla_blog/blog/settings.py
@@ -120,7 +120,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*.herokuapp.com',]
+ALLOWED_HOSTS = ['https://boasbabsblog.herokuapp.com', 'localhost']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
